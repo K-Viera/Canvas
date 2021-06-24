8eb66677ad34f17b0979d28857eb4e79490ad874
programador1 <Kenvi.009@gmail.com>
2021-06-24 15:59:28 -0500

    draw a path

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 var a = document.getElementById("dibujo");
 var ctx = a.getContext("2d");
-ctx.font = "40px Arial"
-ctx.fillText("Prueba",23,40)
+
+var x = a.width/2;
+var y = a.height-30;
+
+var dx = 2;
+var dy = -2;
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
