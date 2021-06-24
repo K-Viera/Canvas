c8f0d970c3de592066eb02fc67ee22a9bd11fbf5
programador1 <Kenvi.009@gmail.com>
2021-06-24 16:23:25 -0500

    bounce ball

--- script.js
+++ script.js
@@ -7,15 +7,29 @@ var y = a.height-30;
 var dx = 2;
 var dy = -2;
 
+var ballRadius = 10;
+
 function draw() {
     ctx.clearRect(0, 0, a.width, a.height);
+    drawBall();
+    
+    if(x + dx > a.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > a.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
-    x += dx;
-    y += dy;
 }
 
 setInterval(draw, 10);
