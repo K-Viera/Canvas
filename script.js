e7ff566aad9166971c14b62bb8f9c2e423ec175e
programador1 <Kenvi.009@gmail.com>
2021-06-28 08:34:59 -0500

    First Example

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-var a = document.getElementById("dibujo");
-var ctx = a.getContext("2d");
-ctx.font = "40px Arial"
-ctx.fillText("Prueba",23,40)
+var svg = d3.select("#svgID")
+var canvas=d3.select("#canvasID");
+var width=parseFloat(canvas.attr("width"));
+var height=parseFloat(canvas.attr("height"));
+var r=3;
+var ctx=canvas.node().getContext("2d");
+
+var graph={
+    nodes:[
+        {name:"Uno"},
+        {name:"dos"}
+    ]
+};
+
+graph.nodes.forEach(function (d){
+    d.x = Math.random()*width;
+    d.y = Math.random()*height;
+});
+
+function update(){
+    ctx.clearRect(0,0,width,height);
+    ctx.beginPath();
+    graph.nodes.forEach(drawNode);
+    ctx.fill();
+}
+
+function drawNode(d){
+    ctx.moveTo(d.x,d.y);
+    ctx.arc(d.x,d.y,r,0,2*Math.PI)
+ }
+
+ update();
